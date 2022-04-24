3d7da8622f74b4bd9be1059a86cc0259f2abce62
Beibarys <beimbik@gmail.com>
2022-04-24 17:12:42 +0600

    Set up transition between menu buttons

--- app/src/main/java/bimbetov/com/example/healthjournal/MainActivity.kt
+++ app/src/main/java/bimbetov/com/example/healthjournal/MainActivity.kt
@@ -2,10 +2,27 @@ package bimbetov.com.example.healthjournal
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+
+        val navController = findNavController(R.id.nav_host_fragment)
+        val appBarConfiguration = AppBarConfiguration(setOf(
+            R.id.eventsFragment,
+            R.id.homeFragment,
+            R.id.profileFragment))
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        navView.setupWithNavController(navController)
     }
 }
